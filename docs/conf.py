99a5ba3c3dc54255b6a8fc323a2fb8ce94a9a5d3
Dennis Scheiba <dennis.scheiba@gmx.de>
2024-01-15 10:44:39 +0100

    switch to sphinx immaterial

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@ author = 'SuperCollider'
 
 extensions = [
     'myst_parser',
+    'sphinx_immaterial',
 ]
 
 source_suffix = {
@@ -28,24 +29,76 @@ root_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'sphinx_material'
+html_title = "SuperCollider wiki"
+html_theme = 'sphinx_immaterial'
 html_static_path = ['_static']
 html_logo = "_static/logo.svg"
 html_css_files = [
     'custom.css',
 ]
-html_theme_options = {
-    "nav_title": "SuperCollider Wiki",
-    'color_primary': 'grey',
-    'color_accent': 'pink',
-    'repo_url': 'https://github.com/supercollider/supercollider',
-    'repo_name': 'SuperCollider',
-    'master_doc': True,
-    'globaltoc_includehidden': True,
-    'globaltoc_depth': 3,
-}
 
-html_sidebars = {
-    "**": ["logo-text.html", "globaltoc.html", "localtoc.html", "searchbox.html"]
+html_theme_options = {
+    "site_url": "https://capital-g.github.io/supercollider-wiki/",
+    "repo_url": "https://github.com/capital-g/supercollider-wiki",
+    "repo_name": "SuperCollider wiki",
+    "edit_uri": "blob/main/docs",
+    "globaltoc_collapse": True,
+    "features": [
+        "navigation.expand",
+        # "navigation.tabs",
+        # "toc.integrate",
+        "navigation.sections",
+        "navigation.instant",
+        # "header.autohide",
+        "navigation.top",
+        # "navigation.tracking",
+        # "search.highlight",
+        "search.share",
+        "toc.follow",
+        "toc.sticky",
+        "content.tabs.link",
+        "announce.dismiss",
+    ],
+    "palette": [
+        {
+            "media": "(prefers-color-scheme: light)",
+            "scheme": "default",
+            "primary": "grey",
+            "accent": "pink",
+            "toggle": {
+                "icon": "material/lightbulb-outline",
+                "name": "Switch to dark mode",
+            },
+        },
+        {
+            "media": "(prefers-color-scheme: dark)",
+            "scheme": "slate",
+            "primary": "grey",
+            "accent": "pink",
+            "toggle": {
+                "icon": "material/lightbulb-outline",
+                "name": "Switch to light mode",
+            },
+        },
+    ],
+    "toc_title_is_page_title": True,
+    "social": [
+        {
+            "icon": "fontawesome/brands/github",
+            "link": "https://github.com/capital-g/supercollider-wiki",
+            "name": "Source on github.com",
+        },
+        {
+            "icon": "fontawesome/brands/discourse",
+            "link": "https://scsynth.org/",
+        },
+        {
+            "icon": "fontawesome/brands/discord",
+            "link": "https://discord.gg/PVUmDyx7p8",
+        },
+        {
+            "icon": "fontawesome/brands/slack",
+            "link": "https://join.slack.com/t/scsynth/shared_invite/zt-ezoyz15j-SVM7JVul94pxtDiUDRnd0w",
+        },
+    ],
 }
